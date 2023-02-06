90cec4cceae3ff272beadef558ebeb89845d653a
gh_masoud <msd.drg@gmail.com>
2023-02-07 03:06:35 +0330

    [BUGFIX] tailwindCSS not affected

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./store";
+import "./index.css";
 
 const el = document.getElementById("root") as HTMLElement;
 const root = createRoot(el);
