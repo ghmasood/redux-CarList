e7178bf65a3218cc706c412ea06d6436d58c570f
gh_masoud <msd.drg@gmail.com>
2023-02-07 21:37:45 +0330

    [IMPROVE] some style

--- src/components/CarList/index.tsx
+++ src/components/CarList/index.tsx
@@ -17,37 +17,41 @@ function CarList() {
     dispatch(removeCar(id));
   };
   return (
-    <div className="w-full flex flex-col gap-2">
-      {AllCars.map((c) => {
-        const isBold =
-          input && c.name.toLowerCase().includes(input.toLowerCase());
-        return (
-          <div
-            key={c.id}
-            className="flex px-2 h-11 items-center bg-sky-50 shadow-sm justify-between rounded-lg"
-          >
-            <span
-              className={`text-2xl ${
-                isBold ? "font-semibold" : "font-normal"
-              } basis-1/3`}
+    <div className="z-20 w-full flex flex-col gap-2 max-h-60 overflow-scroll">
+      {AllCars.length > 0 ? (
+        AllCars.map((c) => {
+          const isBold =
+            input && c.name.toLowerCase().includes(input.toLowerCase());
+          return (
+            <div
+              key={c.id}
+              className="flex px-2 py-1.5  items-center bg-sky-50 shadow-lg justify-between rounded-lg"
             >
-              {c.name}
-            </span>
-            <span className="text-2xl font-light basis-1/3">
-              ${c.cost.toLocaleString()}
-            </span>
-            <button
-              onClick={() => {
-                handleCarDelete(c.id);
-              }}
-              type="submit"
-              className="bg-transparent text-sky-800 border-sky-800 border hover:bg-sky-800 hover:text-white duration-300 px-4 rounded-lg h-8 basis-1/6"
-            >
-              Delete
-            </button>
-          </div>
-        );
-      })}
+              <span
+                className={`text-2xl ${
+                  isBold ? "font-semibold" : "font-normal"
+                } basis-1/3`}
+              >
+                {c.name}
+              </span>
+              <span className="text-2xl font-light basis-1/3">
+                ${c.cost.toLocaleString()}
+              </span>
+              <button
+                onClick={() => {
+                  handleCarDelete(c.id);
+                }}
+                type="submit"
+                className="bg-transparent text-sky-800 border-sky-800 border hover:bg-sky-800 hover:text-white duration-300 px-4 rounded-lg h-8 basis-1/6"
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })
+      ) : (
+        <span className="mx-auto italic text-sky-900">No any car!</span>
+      )}
     </div>
   );
 }
